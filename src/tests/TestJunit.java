56433092b32a8e17d587d8cd50d1a4e9e83fb89f
Rafael Viana <rafael.viana@mv.com.br>
2016-06-07 10:42:19 -0300

    comit

--- src/tests/TestJunit.java
+++ src/tests/TestJunit.java
@@ -1,10 +1,10 @@
-package tests;
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-public class TestJunit {
-   @Test//from  w w w .  ja  v  a2 s.  c o m
-   public void testAdd() {
-      String str= "Junit is working fine";
-      assertEquals("Junit is working fine",str);
-   }
-}
+//package tests;
+//import org.junit.Test;
+//import static org.junit.Assert.assertEquals;
+//public class TestJunit {
+//   @Test//from  w w w .  ja  v  a2 s.  c o m
+//   public void testAdd() {
+//      String str= "Junit is working fine";
+//      assertEquals("Junit is working fine",str);
+//   }
+//}
